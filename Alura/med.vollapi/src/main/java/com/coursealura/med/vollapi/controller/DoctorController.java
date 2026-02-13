be7b840d655d5c4e984088633c32540ceaa396f4
Martins20321 <jgmsilva11@gmail.com>
2026-02-13 19:28:31 -0300

    Sorting and displaying the formatted SQL query

--- Alura/med.vollapi/src/main/java/com/coursealura/med/vollapi/controller/DoctorController.java
+++ Alura/med.vollapi/src/main/java/com/coursealura/med/vollapi/controller/DoctorController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,7 @@ public class DoctorController {
     }
 
     @GetMapping
-    public Page<DoctorListing> findAll(Pageable pageable){
+    public Page<DoctorListing> findAll(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable){
         return repository.findAll(pageable).map(DoctorListing::new);
     }
 }
