0d2c25f93c1da6e076ae93e22d1d01c8318fc935
Martins20321 <jgmsilva11@gmail.com>
2026-02-18 19:10:45 -0300

    refactoring the JSON using Json-databind

--- Alura/adopet.api/src/main/java/br/com/alura/domain/Abrigo.java
+++ Alura/adopet.api/src/main/java/br/com/alura/domain/Abrigo.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class Abrigo implements Serializable {
 
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
@@ -16,6 +17,14 @@ public class Abrigo implements Serializable {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
