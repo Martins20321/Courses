aee2a1ec58c67438b0c2ccaf1940eacadd007cfb
Martins20321 <jgmsilva11@gmail.com>
2026-03-05 22:41:20 -0300

    Abrigo Features

--- Alura/Java/Boas-Práticas/adopet2Rest.api/src/main/java/br/com/alura/adopet/api/model/Abrigo.java
+++ Alura/Java/Boas-Práticas/adopet2Rest.api/src/main/java/br/com/alura/adopet/api/model/Abrigo.java
@@ -1,5 +1,6 @@
 package br.com.alura.adopet.api.model;
 
+import br.com.alura.adopet.api.dto.CadastroAbrigoDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -16,17 +17,20 @@ public class Abrigo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
     private String nome;
 
-    @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
     private String telefone;
 
-    @Email
     private String email;
 
     @OneToMany(mappedBy = "abrigo", cascade = CascadeType.ALL)
     private List<Pet> pets;
+
+    public Abrigo(CadastroAbrigoDTO dto) {
+        this.nome = dto.nome();
+        this.telefone = dto.telefone();
+        this.email = dto.email();
+    }
 
     @Override
     public boolean equals(Object o) {
