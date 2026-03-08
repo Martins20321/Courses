37b1c4ce137efaebdf5783ead1b72c4ad5fe9144
Martins20321 <jgmsilva11@gmail.com>
2026-03-08 20:50:53 -0300

    Adding more scenarios - Probabilidade Baixa

--- Alura/Java/Boas-Práticas/adopet-testautomatizado.api/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ Alura/Java/Boas-Práticas/adopet-testautomatizado.api/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -16,15 +16,18 @@ class CalculadoraProbabilidadeAdocaoTest {
     @DisplayName("Probabilidade alta para gatos jovens e peso baixo")
     void verificarRetornoProbabilidadeAlta() {
 
+        //ARRANGE
         Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo Petz", "99999999991", "petz.abrigo@email.com"));
-        Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "Bob", "Golden", 4, "Ouro", 4.0f), abrigo);
+        Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "Bob", "Siamês", 4, "Ouro", 4.0f), abrigo);
 
         CalculadoraProbabilidadeAdocao calc = new CalculadoraProbabilidadeAdocao();
 
         //Entrando no JUnit (Classe padrão Java para realizar testes automatizados)
+        //ACT
         ProbabilidadeAdocao probabilidadeActual = calc.calcular(pet);
         ProbabilidadeAdocao probabilidadeExpected = ProbabilidadeAdocao.ALTA;
 
+        //ASSERT
         Assertions.assertEquals(probabilidadeExpected, probabilidadeActual);
     }
 
@@ -32,15 +35,35 @@ class CalculadoraProbabilidadeAdocaoTest {
     @DisplayName("Probabilidade média para gatos idosos e peso baixo")
     void verificarRetornoProbabilidadeMedia(){
 
+        //ARRANGE
         Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo Petz", "99999999991", "petz.abrigo@email.com"));
-        Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "Bob", "Golden", 15, "Ouro", 4.0f), abrigo);
+        Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "Bob", "Siamês", 15, "Branca", 4.0f), abrigo);
 
         CalculadoraProbabilidadeAdocao calc = new CalculadoraProbabilidadeAdocao();
 
         //Junit
+        //ACT
         ProbabilidadeAdocao probabilidadeActual = calc.calcular(pet);
         ProbabilidadeAdocao probabilidadeExpected = ProbabilidadeAdocao.MEDIA;
 
+        //ASSERT
+        Assertions.assertEquals(probabilidadeExpected, probabilidadeActual);
+    }
+
+    @Test
+    @DisplayName("Probabilidade baixa para cachorros idosos e peso alto")
+    void verificarProbabilidadeBaixa(){
+
+        //ARRANGE
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo Petz", "99999999991", "petz.abrigo@email.com"));
+        Pet pet = new Pet(new CadastroPetDto(TipoPet.CACHORRO, "Snoop", "Golden", 16, "Laranja", 18.0f), abrigo);
+        CalculadoraProbabilidadeAdocao calc = new CalculadoraProbabilidadeAdocao();
+
+        //ACT
+        ProbabilidadeAdocao probabilidadeActual = calc.calcular(pet);
+        ProbabilidadeAdocao probabilidadeExpected = ProbabilidadeAdocao.BAIXA;
+
+        //ASSERT
         Assertions.assertEquals(probabilidadeExpected, probabilidadeActual);
     }
 }
