6156b0627b468eb9f5d7e5867ec1a7cbf7adaabb
Martins20321 <jgmsilva11@gmail.com>
2026-03-14 00:41:48 -0300

    Test the Controller layer - AdocaoController - Ok 200

--- Alura/Java/Boas-Práticas/adopet-testautomatizado.api/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ Alura/Java/Boas-Práticas/adopet-testautomatizado.api/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,11 +1,13 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,6 +19,8 @@ class AdocaoControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean //Anotação do próprio Spring
+    private AdocaoService service;
 
     @Test
     @DisplayName("Deve retornar código 400 ao solicitar adoção com erro")
@@ -35,5 +39,28 @@ class AdocaoControllerTest {
 
         //ASSERTIVE
         Assertions.assertEquals(400,response.getStatus());
+    }
+
+    @Test
+    @DisplayName("Deve retornar código 200 ao solicitar adoção com Sucesso!")
+    void verificacaoDeSucessoSolicitacao() throws Exception {
+
+        //ARRANGE
+        String json = """
+                {
+                    "idPet": 1,
+                    "idTutor": 1,
+                    "motivo": "Motivo Qualquer"
+                }
+                """;
+        //ACT
+        var response = mockMvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+        
+        //ASSERTIVE
+        Assertions.assertEquals(200, response.getStatus());
     }
 }
