06d8ac73dd807f866c3eb29f73e0625de466387d
Martins20321 <jgmsilva11@gmail.com>
2026-04-23 12:10:03 -0300

    Retrieving token and verifying if it is being sent in the header

--- Alura/Java/Spring Boot 3/Med-Voll-API/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ Alura/Java/Spring Boot 3/Med-Voll-API/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -15,7 +15,21 @@ public class SecurityFilter extends OncePerRequestFilter {
     //Garante que vai ser executada uma única vez antes de cada requisição
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //Recuperado o cabeçalho
+        var tokenJWT = recuperarToken(request);
+
+        System.out.println(tokenJWT);
         //Continuar o fluxo da requisição
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null){
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+
+        return authorizationHeader.replace("Bearer ","");
     }
 }
