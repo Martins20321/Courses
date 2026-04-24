025e040c7f0fbf906f090343f4b1ec302de2fe1c
Martins20321 <jgmsilva11@gmail.com>
2026-04-24 11:15:02 -0300

    token validation process

--- Alura/Java/Spring Boot 3/Med-Voll-API/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ Alura/Java/Spring Boot 3/Med-Voll-API/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,13 +4,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
 
     //Garante que vai ser executada uma única vez antes de cada requisição
     @Override
@@ -19,7 +23,8 @@ public class SecurityFilter extends OncePerRequestFilter {
         //Recuperado o cabeçalho
         var tokenJWT = recuperarToken(request);
 
-        System.out.println(tokenJWT);
+        var subject = tokenService.getSubject(tokenJWT);
+
         //Continuar o fluxo da requisição
         filterChain.doFilter(request, response);
     }
--- Alura/Java/Spring Boot 3/Med-Voll-API/src/main/java/med/voll/api/infra/security/TokenService.java
+++ Alura/Java/Spring Boot 3/Med-Voll-API/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,8 +1,10 @@
 package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +32,20 @@ public class TokenService {
                     .sign(algoritmo); //Assinatura
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    //Processo de Validação do Token
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API voll med") //Quem gerou o token
+                    .build()
+                    .verify(tokenJWT) //Verifica o token. Inválido = Exceção, Válido = Continua a execução
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
