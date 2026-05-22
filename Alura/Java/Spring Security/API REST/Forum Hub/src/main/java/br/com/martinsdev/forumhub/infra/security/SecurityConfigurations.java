e0a84835b83bda18303e207f99b7b69357e58838
Martins20321 <jgmsilva11@gmail.com>
2026-05-22 11:20:35 -0300

    configuring for a stateless API and disabling CSRF protection

--- Alura/Java/Spring Security/API REST/Forum Hub/src/main/java/br/com/martinsdev/forumhub/infra/security/SecurityConfigurations.java
+++ Alura/Java/Spring Security/API REST/Forum Hub/src/main/java/br/com/martinsdev/forumhub/infra/security/SecurityConfigurations.java
@@ -2,13 +2,23 @@ package br.com.martinsdev.forumhub.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        return security.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .sessionManagement(sessionManagement
+                        -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }
 
     //Utilizando BCrypt para criptografia de senhas
     @Bean
