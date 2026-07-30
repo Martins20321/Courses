3adc694a78847f84c83c39a3316950e7b888c1e5
Martins20321 <jgmsilva11@gmail.com>
2026-07-30 11:11:07 -0300

    feat: create VPC with public and private subnets using CDK

---
+++ Alura/Arquitetura de Microsserviços/alura-aws-infra/src/main/java/com/myorg/AluraVpcStack.java
@@ -0,0 +1,21 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+public class AluraVpcStack extends Stack {
+    public AluraVpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public AluraVpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        //Criando a VPC
+        Vpc vpc = Vpc.Builder.create(this, "AluraVpc")
+                .maxAzs(3)  // Default is all AZs in region. A VPC vai abranger até 3 AZs
+                .build();
+    }
+}
