2fbd19453905726e0ca499fa751a570302251701
Martins20321 <jgmsilva11@gmail.com>
2026-08-02 20:05:07 -0300

    feat: add CloudWatch Logs monitoring to pedidos-ms ECS service

--- Alura/Arquitetura de Microsserviços/alura-aws-infra/src/main/java/com/myorg/AluraServiceStack.java
+++ Alura/Arquitetura de Microsserviços/alura-aws-infra/src/main/java/com/myorg/AluraServiceStack.java
@@ -1,14 +1,18 @@
 package com.myorg;
 
 import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 import java.util.Map;
@@ -29,7 +33,7 @@ public class AluraServiceStack extends Stack {
                 .cluster(cluster)           // Required
                 //Task Definition
                 .cpu(512)                   // Default is 256
-                .desiredCount(1)      // Quantidade de instâncias desejadas
+                .desiredCount(3)      // Quantidade de instâncias desejadas
                 .listenerPort(8080)     //Porta que o LB escuta
                 .assignPublicIp(true)   //Atribui IP público ao container
                 .taskImageOptions(
@@ -43,6 +47,13 @@ public class AluraServiceStack extends Stack {
                                                 + ":3306/alurafood-pedidos?createDatabaseIfNotExist=true",
                                         "SPRING_DATASOURCE_USERNAME","admin",
                                         "SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-senha")))
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                                .logGroup(LogGroup.Builder.create(this, "PedidosMsLogGroup")
+                                                        .logGroupName("PedidosMsLog") //Nome do grupo no CloudWatch
+                                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                                        .build())
+                                                .streamPrefix("PedidosMs")
+                                        .build()))
                                 .build())
                 .memoryLimitMiB(1024)       // Default is 512
                 .publicLoadBalancer(true)   // LB acessível pela internet
