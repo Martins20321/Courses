4587681872cd99aef7def91ec8ad8ca242dedb2b
Martins20321 <jgmsilva11@gmail.com>
2026-08-13 11:11:47 -0300

    feat(pedidos-ms): deserialize payment events as JSON with Jackson2JsonMessageConverter

--- Alura/Arquitetura de Microsserviços/alurafood/pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ Alura/Arquitetura de Microsserviços/alurafood/pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,6 +1,6 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
+import br.com.alurafood.pedidos.event.PagamentoConcluidoEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +9,14 @@ public class PagamentoListener {
 
     //Metodo para receber as mensagens
     @RabbitListener(queues = "pagamento.concluido") //Quero receber mensagens dessa fila. Listener = Se torna um ouvinte desta fila
-    public void receberMensagens(Message message) {
-        System.out.println("Recebido a mensagem " + message.toString());
+    public void receberMensagens(PagamentoConcluidoEvent pagamento) {
+        //Formatação da mensagem, como vou receber
+        String message = """
+                Dados do Pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s
+                """.formatted(pagamento.id(), pagamento.pedidoId(), pagamento.valor(), pagamento.status());
+        System.out.println("Recebido a mensagem " + message);
     }
 }
